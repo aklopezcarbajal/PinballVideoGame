6d11fae2637d1117ce235086e6a73860b1e2a54d
karenlc <aklopezcarbajal@gmail.com>
2020-07-12 16:55:47 -0400

    Pinball Unity3D project.

---
+++ Assets/Scripts/FlipperController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlipperController : MonoBehaviour
+{
+    private HingeJoint hinge;
+    private string keyName;
+    private bool rightFlipper = false;
+    private bool leftFlipper  = false;
+
+    void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        if (transform.position.x < 0)
+        {
+            leftFlipper = true;
+            keyName = "left";
+        }
+        if (transform.position.x > 0)
+        {
+            rightFlipper = true;
+            keyName = "right";
+        }
+    }
+
+    void Update()
+    {
+        JointSpring spring = hinge.spring;
+
+        if (Input.GetKey(keyName))
+        {
+            float val = (leftFlipper == true ? 1f : -1f);
+            spring.spring = 10000;
+            spring.damper = 30;
+            spring.targetPosition = (-70)*val;
+        }
+        else
+        {
+            spring.targetPosition = 0f;
+        }
+        hinge.spring = spring;
+    }
+}
