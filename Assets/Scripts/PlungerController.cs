717a653890875e5dff2d36ecc0b892a19c54dd79
karenlc <aklopezcarbajal@gmail.com>
2020-07-16 15:26:06 -0400

    Add plunger controller script.
    The trigger detects when the ball is in position. The force increases while the player holds the space key down. Adds a force to the ball when the key is released.

---
+++ Assets/Scripts/PlungerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlungerController : MonoBehaviour
+{
+    private Rigidbody ballRB;
+    private bool ballReady;
+    private float force;
+    private float minForce = 0f;
+    private float maxForce = 10000f;
+
+
+    void Start()
+    {
+        ballRB = GameObject.Find("Ball").GetComponent<Rigidbody>();
+        ballReady = false;
+        force = 0f;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        ballReady = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        ballReady = false;
+    }
+    
+    void Update()
+    {
+        if (Input.GetKey("space") && ballReady)
+        {
+            if (force < maxForce) 
+                force += 500f * Time.deltaTime;
+        }
+        if (Input.GetKeyUp("space") && ballReady)
+        {
+            print("Hit ball.");
+            print(force);
+            ballRB.AddForce( Vector3.forward * force);
+            force = 0f;
+        }
+        
+    }
+}
