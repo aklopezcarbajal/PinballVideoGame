5c4d589ecc4ab103dd488b4737da6ac553c44c21
karenlc <aklopezcarbajal@gmail.com>
2020-07-19 17:37:17 -0400

    Add switchers.

---
+++ Assets/Scripts/SwitchState.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchState : MonoBehaviour
+{
+    public bool isOn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isOn = false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (isOn)
+            isOn = false;
+        else
+            isOn = true;
+        print("Switch state");
+        if (isOn)
+            print("Is ON");
+        else
+            print("Is OFF");
+    }
+}
