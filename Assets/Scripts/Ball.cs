94993a517aa2b31de2252b4b3f02de9729506828
karenlc <aklopezcarbajal@gmail.com>
2020-07-24 19:44:52 -0400

    Change structure of playfield. Adjust values of Ball script.
    Delete tunnel and a few bumpers. Add walls and corners. Change shape of slingshots.

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class Ball : MonoBehaviour
 {
+    public int maxBalls = 3;
     private Vector3 initialPosition;
-    private float lowerBound = -25;
+    public float lowerBound = -20;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +16,9 @@ public class Ball : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.z < lowerBound)
+        if (transform.position.z < lowerBound && maxBalls > 0)
         {
+            maxBalls -= 1;
             transform.position = initialPosition;
         }
     }
