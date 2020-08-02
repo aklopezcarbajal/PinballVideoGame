3e1b026fd58937b6c9930345eda798f389fd5427
karenlc <aklopezcarbajal@gmail.com>
2020-08-02 13:00:40 -0400

    Add score text and background. Add switch next to slingshots.

--- Assets/Scripts/Score.cs
+++ Assets/Scripts/Score.cs
@@ -1,10 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class Score : MonoBehaviour
 {
     public int count;
+    public Text scoreText;
+    private string scoreStr;
+    private int MAXSCORE = 9999999;
 
     // Start is called before the first frame update
     void Start()
@@ -14,10 +18,13 @@ public class Score : MonoBehaviour
 
     void Update()
     {
+        scoreStr = count.ToString();
+        scoreText.text = scoreStr.PadLeft(7,'0');
     }
 
     public void IncreaseScore(int val)
     {
-        count += val;
+        if(count + val < MAXSCORE)
+            count += val;
     }
 }
